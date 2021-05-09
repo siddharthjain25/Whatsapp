e3f58db97cb0c409fa10315bf77c148415f2edc5
siddharthjain25 <siddharthjainj25@gmail.com>
2021-05-09 21:04:07 +0530

    whatsapp

---
+++ public/Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAOnd6lQ57l6ao1EzIotKX-25QQgcH6GrQ",
+    authDomain: "whatsapp-4b28f.firebaseapp.com",
+    databaseURL: "https://whatsapp-4b28f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "whatsapp-4b28f",
+    storageBucket: "whatsapp-4b28f.appspot.com",
+    messagingSenderId: "69659230475",
+    appId: "1:69659230475:web:2a51d855b58d933724220e",
+    measurementId: "G-1NTHWJJ5MD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ public/messaging/firebase-messaging-sw.js
@@ -0,0 +1,40 @@
+﻿// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAOnd6lQ57l6ao1EzIotKX-25QQgcH6GrQ",
+    authDomain: "whatsapp-4b28f.firebaseapp.com",
+    databaseURL: "https://whatsapp-4b28f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "whatsapp-4b28f",
+    storageBucket: "whatsapp-4b28f.appspot.com",
+    messagingSenderId: "69659230475",
+    appId: "1:69659230475:web:2a51d855b58d933724220e",
+    measurementId: "G-1NTHWJJ5MD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'You have new message';
+    const notificationOptions = {
+        body: payload.data.message,
+        icon: payload.data.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+
+
